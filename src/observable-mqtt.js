f34db79cd61c9736e6dbb50dfac51c2670b31e5d
Jamie Nuttall <jamie@wattu.com>
2017-09-16 11:26:23 +0100

    share same messages obs

--- src/observable-mqtt.js
+++ src/observable-mqtt.js
@@ -4,12 +4,14 @@ import _mqtt from 'mqtt'
 const ObservableMqtt = ({ mqtt = _mqtt, uri, options = {} }) => topic => {
     const client = mqtt.connect(uri, options)
     client.subscribe(topic)
+    const messages = Observable.fromEvent(client, 'message', (topic, message) => ({ topic, message }))
+        .filter(msg => msg.topic === topic)
+        .map(msg => msg.message)
     return {
         send: message => client.publish(topic, message),
-        messages: () => Observable.fromEvent(client, 'message', (topic, message) => ({ topic, message }))
-            .filter(msg => msg.topic === topic)
-            .map(msg => msg.message)
+        messages: () => messages,
     }
 }
+
 
 export default ObservableMqtt
